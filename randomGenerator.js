72212993454d15a68e2f286d73cff93212910b40
Kareem Salem <kareemsalem736@gmail.com>
2025-03-09 18:36:32 -0500

    Completed randomGenerator program

--- randomGenerator.js
+++ randomGenerator.js
@@ -1,1 +1,18 @@
-console.log('randomGenerator.js');
+/*
+Kareem Salem    
+Git, Github, and Node test
+*/
+
+/*
+This program generates a random sentence with three different random words from three different arrays.
+*/
+
+const arr1 = ['Giraffe', 'Elephant', 'Lion', 'Tiger', 'Zebra'];
+const arr2 = ['house', 'car', 'bike', 'plane', 'boat'];
+const arr3 = ['happy', 'sad', 'angry', 'excited', 'bored'];
+
+const randomWord1 = arr1[Math.floor(Math.random() * arr1.length)];
+const randomWord2 = arr2[Math.floor(Math.random() * arr2.length)];
+const randomWord3 = arr3[Math.floor(Math.random() * arr3.length)];
+
+console.log(`The ${randomWord3} ${randomWord1} went to the ${randomWord2}.`);
