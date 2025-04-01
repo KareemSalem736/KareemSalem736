bf984d6671fe944aed6fb12c39e8567862ba872c
Kareem Salem <kareemsalem736@gmail.com>
2025-04-01 14:59:17 -0500

    Adjusted website and added bakground photo

--- WebsiteCodecademy/script.js
+++ WebsiteCodecademy/script.js
@@ -1,3 +1,24 @@
-const popup = document.getElementById("popup");
-const btn document.getElementById("open-popup");
-const 
+const buttons = document.querySelectorAll("button[id^='open-popup']");
+const popups = document.querySelectorAll(".popup");
+const closes = document.querySelectorAll(".close");
+
+buttons.forEach((btn) => {
+    btn.onclick = () => {
+        const id = btn.id.replace("open-", "");
+        document.getElementById(id).style.display = "block";  
+    };
+});
+
+closes.forEach((span) => {
+    span.onclick = () => {
+        span.closest(".popup").style.display = "none";
+    };
+});
+
+window.onclick = (event) => {
+    popups.forEach((popup) => {
+        if (event.target === popup) {
+            popup.style.display = "none";
+        }
+    });
+};
