bdc740a7515ab4d80b61c4faa7969b2123187880
Kareem Salem <kareemsalem736@gmail.com>
2025-05-06 09:40:44 -0500

    played with useState in ranom generator

--- github_project/random_generator/randomGenerator.js
+++ github_project/random_generator/randomGenerator.js
@@ -16,3 +16,22 @@ const randomWord2 = arr2[Math.floor(Math.random() * arr2.length)];
 const randomWord3 = arr3[Math.floor(Math.random() * arr3.length)];
 
 console.log(`The ${randomWord3} ${randomWord1} went to the ${randomWord2}.`);
+
+//react statehook
+import React, {useState} from 'react';
+
+const lightSwitch = () => {
+    const [light, setLight] = useState('Off');
+
+    const toggleLight = () => {
+        setLight(prev => prev === 'Off' ? 'On' : 'Off');
+    }
+    return (
+        <div>
+            <p>Light is {light}</p>
+            <button onClick={toggleLight}></button>
+        </div>
+    );
+};
+
+export default lightSwitch;
