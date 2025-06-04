730abd03b31951826bcc78265935c54f6e25c5aa
Kareem Salem <kareem@KareemSalem.>
2025-06-03 20:52:42 -0500

    started project 1 benchmark file

--- CMSC451/project1/Benchmark.java
+++ CMSC451/project1/Benchmark.java
@@ -1,18 +1,76 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
 
-public class Benchmark{
+public class Benchmark {
     public static void main(String[] args) {
-    System.out.println("Hello World");
-    List<Integer> bubble = new ArrayList<>();
-    bubbleSort(bubble);
-    System.out.println(bubble);
-    }
-    public List bubbleSort(List bubble) {
-        for (int i = 0; i < 5; i++) {
-            bubble.add(i);
+        Random rand = new Random();
+        BubbleSort bubbleSort = new BubbleSort();
+        QuickSort quickSort = new QuickSort();
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = rand.nextInt(10000);
         }
-        return bubble;
+        for (int i = 0; i <= 40; i++) {
+            quickSort.sort(array);
+            quickSort.getCount();
+            quickSort.getTime();
+
+            bubbleSort.sort(array);
+            bubbleSort.getCount();
+            bubbleSort.getTime();
+        }
     }
 }
-  
+
+class BubbleSort extends AbstractSort {
+    public int[] sort(int[] array) {
+        int temp;
+        startSort();
+        for (int i = 0; i < array.length - 1; i++) {
+            for (int j = 0; j < array.length - i - 1; j++) {
+                if (array[j] > array[j + 1]) {
+                    temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+                incrementCount();
+            }
+        }
+        endSort();
+        return array;
+    }
+}
+
+class QuickSort extends AbstractSort {
+
+}
+
+abstract class AbstractSort {
+
+    private int count;
+    private long startTime;
+    private long endTime;
+
+    public abstract int[] sort(int[] array);
+
+    void startSort() {
+        count = 0;
+        startTime = System.nanoTime();
+    }
+
+    void endSort() {
+        endTime = System.nanoTime();
+    }
+
+    void incrementCount() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public long getTime() {
+        return endTime - startTime;
+    }
+}
